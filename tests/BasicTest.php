07d63ff7dece281d017c8193b5126dc4139c1ad5
Sylvain UTARD <sylvain.utard@gmail.com>
2014-01-20 10:20:33 -0800

    Travis integration

---
+++ tests/BasicTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class BasicTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+    }
+
+    public function testTruth()
+    {
+        $this->assertTrue(true);
+    }
+
+}

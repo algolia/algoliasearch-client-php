e58a3e1a886036698f30cc19a715b8e2d0dc8406
Xavier Grand <grand.xavier@gmail.com>
2015-05-27 14:34:51 +0200

    Add test about facetFilters and Fix issue with nested array

--- src/AlgoliaSearch/Index.php
+++ src/AlgoliaSearch/Index.php
@@ -291,6 +291,11 @@ class Index {
         if ($args === null) {
             $args = array();
         }
+        foreach ($args as $key => $value) {
+          if (gettype($value) == "array") {
+            $args[$key] = json_encode($value);
+          }
+        }
         $args["query"] = $query;
         return $this->client->request($this->context, "POST", "/1/indexes/" . $this->urlIndexName . "/query", array(), array("params" => http_build_query($args)), $this->context->readHostsArray, $this->context->connectTimeout, $this->context->searchTimeout);
     }

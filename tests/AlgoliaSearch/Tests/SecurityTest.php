e0fc6aa8bcf4e5a8683a5431a00b0d326963fdf7
Jan Petr <honzik.petr@gmail.com>
2016-05-09 19:16:50 +0200

    Fix Security tests. Fixes #78

--- tests/AlgoliaSearch/Tests/SecurityTest.php
+++ tests/AlgoliaSearch/Tests/SecurityTest.php
@@ -4,10 +4,14 @@ namespace AlgoliaSearch\Tests;
 
 use AlgoliaSearch\AlgoliaException;
 use AlgoliaSearch\Client;
+use AlgoliaSearch\Index;
 
 class SecurityTest extends AlgoliaSearchTestCase
 {
+    /** @var Client */
     private $client;
+
+    /** @var Index */
     private $index;
 
     protected function setUp()
@@ -34,41 +38,73 @@ class SecurityTest extends AlgoliaSearchTestCase
     {
         $res = $this->index->addObject(['firstname' => 'Robin']);
         $this->index->waitTask($res['taskID']);
+
         $res = $this->index->listUserKeys();
         $newKey = $this->index->addUserKey(['search']);
-        sleep(5);
-        $this->assertTrue($newKey['key'] != '');
-        $resAfter = $this->index->listUserKeys();
-        $this->assertTrue($this->containsValue($resAfter['keys'], 'value', $newKey['key']));
-        $this->assertFalse($this->containsValue($res['keys'], 'value', $newKey['key']));
-        $key = $this->index->getUserKeyACL($newKey['key']);
-        $this->assertEquals($key['acl'][0], 'search');
-        $this->index->updateUserKey($newKey['key'], ['addObject']);
-        sleep(5);
-        $key = $this->index->getUserKeyACL($newKey['key']);
-        $this->assertEquals($key['acl'][0], 'addObject');
-        $this->index->deleteUserKey($newKey['key']);
-        sleep(5);
-        $resEnd = $this->index->listUserKeys();
-        $this->assertFalse($this->containsValue($resEnd['keys'], 'value', $newKey['key']));
 
-        $res = $this->client->listUserKeys();
-        $newKey = $this->client->addUserKey(['search']);
-        sleep(5);
         $this->assertTrue($newKey['key'] != '');
-        $resAfter = $this->client->listUserKeys();
-        $this->assertTrue($this->containsValue($resAfter['keys'], 'value', $newKey['key']));
         $this->assertFalse($this->containsValue($res['keys'], 'value', $newKey['key']));
-        $key = $this->client->getUserKeyACL($newKey['key']);
+
+        $this->poolingTask(function ($timeouted) use ($newKey) {
+            $resAfter = $this->index->listUserKeys();
+
+            if ($this->containsValue($resAfter['keys'], 'value', $newKey['key']) || time() >= $timeouted) {
+                $this->assertTrue($this->containsValue($resAfter['keys'], 'value', $newKey['key']));
+
+                return true;
+            }
+
+            return false;
+        });
+
+        $key = $this->poolingTask(function ($timeouted) use ($newKey) {
+            try {
+                $key = $this->index->getUserKeyACL($newKey['key']);
+                return $key;
+            } catch (AlgoliaException $e) {
+                if (time() >= $timeouted) {
+                    throw $e;
+                }
+            }
+
+            return false;
+        });
+
         $this->assertEquals($key['acl'][0], 'search');
-        $this->client->updateUserKey($newKey['key'], ['addObject']);
-        sleep(5);
-        $key = $this->client->getUserKeyACL($newKey['key']);
-        $this->assertEquals($key['acl'][0], 'addObject');
-        $this->client->deleteUserKey($newKey['key']);
-        sleep(5);
-        $resEnd = $this->client->listUserKeys();
-        $this->assertFalse($this->containsValue($resEnd['keys'], 'value', $newKey['key']));
+        
+        $this->index->updateUserKey($newKey['key'], ['addObject']);
+
+        $this->poolingTask(function ($timeouted) use ($newKey) {
+            try {
+                $key = $this->index->getUserKeyACL($newKey['key']);
+
+                if ($key['acl'][0] === 'addObject' || time() >= $timeouted) {
+                    $this->assertEquals($key['acl'][0], 'addObject');
+
+                    return true;
+                }
+            } catch (AlgoliaException $e) {
+                if (time() >= $timeouted) {
+                    throw $e;
+                }
+            }
+
+            return false;
+        });
+
+        $this->index->deleteUserKey($newKey['key']);
+
+        $this->poolingTask(function ($timeouted) use ($newKey) {
+            $resEnd = $this->index->listUserKeys();
+
+            if ($this->containsValue($resEnd['keys'], 'value', $newKey['key']) === false || time() >= $timeouted) {
+                $this->assertFalse($this->containsValue($resEnd['keys'], 'value', $newKey['key']));
+
+                return true;
+            }
+
+            return false;
+        });
     }
 
     public function testSecurityMultipleIndices()
@@ -81,10 +117,19 @@ class SecurityTest extends AlgoliaSearchTestCase
         $b->waitTask($res['taskID']);
 
         $newKey = $this->client->addUserKey(['search', 'addObject', 'deleteObject'], 0, 0, 0, [$this->safe_name('a-12'), $this->safe_name('b-13')]);
-        sleep(5);
         $this->assertTrue($newKey['key'] != '');
-        $res = $this->client->listUserKeys();
-        $this->assertTrue($this->containsValue($res['keys'], 'value', $newKey['key']));
+
+        $this->poolingTask(function ($timeouted) use ($newKey) {
+            $res = $this->client->listUserKeys();
+
+            if ($this->containsValue($res['keys'], 'value', $newKey['key']) || time() >= $timeouted) {
+                $this->assertTrue($this->containsValue($res['keys'], 'value', $newKey['key']));
+
+                return true;
+            }
+
+            return false;
+        });
 
         $this->client->deleteIndex($this->safe_name('a-12'));
         $this->client->deleteIndex($this->safe_name('b-13'));
@@ -96,5 +141,20 @@ class SecurityTest extends AlgoliaSearchTestCase
         $this->assertEquals('MDZkNWNjNDY4M2MzMDA0NmUyNmNkZjY5OTMzYjVlNmVlMTk1NTEwMGNmNTVjZmJhMmIwOTIzYjdjMTk2NTFiMnRhZ0ZpbHRlcnM9JTI4cHVibGljJTJDdXNlcjElMjk=', $this->client->generateSecuredApiKey('182634d8894831d5dbce3b3185c50881', ['tagFilters' => '(public,user1)']));
         $this->assertEquals('OGYwN2NlNTdlOGM2ZmM4MjA5NGM0ZmYwNTk3MDBkNzMzZjQ0MDI3MWZjNTNjM2Y3YTAzMWM4NTBkMzRiNTM5YnRhZ0ZpbHRlcnM9JTI4cHVibGljJTJDdXNlcjElMjkmdXNlclRva2VuPTQy', $this->client->generateSecuredApiKey('182634d8894831d5dbce3b3185c50881', ['tagFilters' => '(public,user1)', 'userToken' => '42']));
         $this->assertEquals('OGYwN2NlNTdlOGM2ZmM4MjA5NGM0ZmYwNTk3MDBkNzMzZjQ0MDI3MWZjNTNjM2Y3YTAzMWM4NTBkMzRiNTM5YnRhZ0ZpbHRlcnM9JTI4cHVibGljJTJDdXNlcjElMjkmdXNlclRva2VuPTQy', $this->client->generateSecuredApiKey('182634d8894831d5dbce3b3185c50881', ['tagFilters' => '(public,user1)'], '42'));
+    }
+
+    private function poolingTask(\Closure $callback, $timeout = 200)
+    {
+        $timeouted = time() + $timeout;
+        while (time() < $timeouted) {
+            sleep(1);
+
+            $res = $callback($timeouted);
+            if ($res !== false) {
+                return $res;
+            }
+        }
+
+        return null;
     }
 }

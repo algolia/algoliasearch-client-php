0be6a47f736f3366a074d4c9b1a446f0d6b8bd1b
Jan Petr <honzik.petr@gmail.com>
2016-05-16 19:13:19 +0200

    Fix AccessTest to be resistant against DSN attack and remove hardcoded hosts from testAccessWithOptions

--- tests/AlgoliaSearch/Tests/AccessTest.php
+++ tests/AlgoliaSearch/Tests/AccessTest.php
@@ -8,20 +8,28 @@ class AccessTest extends AlgoliaSearchTestCase
 {
     public function testHTTPAccess()
     {
-        $client = new Client(getenv('ALGOLIA_APPLICATION_ID'), getenv('ALGOLIA_API_KEY'), ['http://'.getenv('ALGOLIA_APPLICATION_ID').'-1.algolia.io']);
+        $client = new Client(getenv('ALGOLIA_APPLICATION_ID'), getenv('ALGOLIA_API_KEY'), [
+            'http://'.getenv('ALGOLIA_APPLICATION_ID').'-dsn.algolia.net',
+            'http://'.getenv('ALGOLIA_APPLICATION_ID').'-1.algolianet.com'
+        ]);
+
         $client->isAlive();
     }
 
     public function testHTTPSAccess()
     {
-        $client = new Client(getenv('ALGOLIA_APPLICATION_ID'), getenv('ALGOLIA_API_KEY'), ['https://'.getenv('ALGOLIA_APPLICATION_ID').'-1.algolia.io']);
+        $client = new Client(getenv('ALGOLIA_APPLICATION_ID'), getenv('ALGOLIA_API_KEY'), [
+            'https://'.getenv('ALGOLIA_APPLICATION_ID').'-dsn.algolia.net',
+            'https://'.getenv('ALGOLIA_APPLICATION_ID').'-1.algolianet.com'
+        ]);
+
         $client->isAlive();
     }
 
     public function testAccessWithOptions()
     {
-        $client = new Client(getenv('ALGOLIA_APPLICATION_ID'), getenv('ALGOLIA_API_KEY'), ['https://'.getenv('ALGOLIA_APPLICATION_ID').'-1.algolia.io'],
-                                ['curloptions' => ['CURLOPT_FAILONERROR' => 0]]);
+        $client = new Client(getenv('ALGOLIA_APPLICATION_ID'), getenv('ALGOLIA_API_KEY'), null, ['curloptions' => ['CURLOPT_FAILONERROR' => 0]]);
+        
         $client->isAlive();
     }
 }

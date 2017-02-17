bd081334734c192af0f0a7efa7e8bd2d57b7283c
Maxime Locqueville <maxiloc@gmail.com>
2017-02-17 14:26:30 +0100

    Fix duplicate user agent and allow more than one user agent prefix

--- tests/AlgoliaSearch/Tests/VersionTest.php
+++ tests/AlgoliaSearch/Tests/VersionTest.php
@@ -6,7 +6,14 @@ use AlgoliaSearch\Version;
 
 class VersionTest extends AlgoliaSearchTestCase
 {
-    public function testVersion()
+    public function tearDown()
+    {
+        Version::$custom_value = '';
+        Version::clearUserAgentSuffixesAndPrefixes();
+    }
+
+
+    public function testVersionAddOnePrefixAndOneSuffix()
     {
         $userAgent = Version::getUserAgent();
         $this->assertRegExp('/^Algolia for PHP \(\d+\.\d+\.\d+\)$/', $userAgent);
@@ -14,26 +21,63 @@ class VersionTest extends AlgoliaSearchTestCase
         Version::addPrefixUserAgentSegment('Prefix integration', '0.0.8');
         Version::addSuffixUserAgentSegment('Suffix platform', '1.2.3');
 
-        // Should be "Prefix 0.0.8; Algolia for PHP (X.Y.Z); Suffix platform (1.2.3)"
         $userAgent = Version::getUserAgent();
-        $this->assertRegExp('/^Prefix integration \(0\.0\.8\); Algolia for PHP \(\d+\.\d+\.\d+\); Suffix platform \(1\.2\.3\)$/', $userAgent);
+        $this->assertEquals('Prefix integration (0.0.8); Algolia for PHP ('.Version::VALUE.'); Suffix platform (1.2.3)', $userAgent);
+    }
 
+    public function testVersionAddTwoPrefixAndTwoSuffix()
+    {
+        Version::addPrefixUserAgentSegment('Prefix integration', '0.0.8');
+        Version::addSuffixUserAgentSegment('Suffix platform', '1.2.3');
         Version::addPrefixUserAgentSegment('Another prefix', '5.6.7');
         Version::addSuffixUserAgentSegment('Different suffix', '7.8.9');
 
-        // Should be "Another prefix (5.6.7); Prefix 0.0.8; Algolia for PHP (X.Y.Z); Suffix platform (1.2.3); Different suffix (7.8.9)"
         $userAgent = Version::getUserAgent();
-        $this->assertRegExp('/^Another prefix \(5\.6\.7\); Prefix integration \(0\.0\.8\); Algolia for PHP \(\d+\.\d+\.\d+\); Suffix platform \(1\.2\.3\); Different suffix \(7\.8\.9\)$/', $userAgent);
+        $this->assertEquals('Another prefix (5.6.7); Prefix integration (0.0.8); Algolia for PHP ('.Version::VALUE.'); Suffix platform (1.2.3); Different suffix (7.8.9)', $userAgent);
 
         // Should be "X.Y.Z"
         $version = Version::get();
         $this->assertRegExp('/^\d+\.\d+\.\d+$/', $version);
+    }
 
+    public function testVersionAddTwoPrefixAndTwoSuffixAndCustomValue()
+    {
+        Version::addPrefixUserAgentSegment('Prefix integration', '0.0.8');
+        Version::addSuffixUserAgentSegment('Suffix platform', '1.2.3');
+        Version::addPrefixUserAgentSegment('Another prefix', '5.6.7');
+        Version::addSuffixUserAgentSegment('Different suffix', '7.8.9');
         Version::$custom_value = ' custom_value';
 
         $this->assertStringEndsWith(' custom_value', Version::getUserAgent());
 
         // Shoul be "X.Y.Z custom_value"
         $this->assertRegExp('/^\d+\.\d+\.\d+ custom_value$/', Version::get());
+    }
+
+    public function testVersionDuplicatesPrefix()
+    {
+        Version::addPrefixUserAgentSegment('Another prefix', '5.6.7');
+        Version::addPrefixUserAgentSegment('Another prefix', '5.6.7');
+
+        $userAgent = Version::getUserAgent();
+        $this->assertEquals('Another prefix (5.6.7); Algolia for PHP ('.Version::VALUE.')', $userAgent);
+    }
+
+    public function testVersionDuplicatesSuffix()
+    {
+        Version::addSuffixUserAgentSegment('Another suffix', '5.6.7');
+        Version::addSuffixUserAgentSegment('Another suffix', '5.6.7');
+
+        $userAgent = Version::getUserAgent();
+        $this->assertEquals('Algolia for PHP ('.Version::VALUE.'); Another suffix (5.6.7)', $userAgent);
+    }
+
+    public function testVersionTwoPrefix()
+    {
+        Version::addPrefixUserAgentSegment('prefix', '5.6.7');
+        Version::addPrefixUserAgentSegment('Another prefix', '5.6.7');
+
+        $userAgent = Version::getUserAgent();
+        $this->assertEquals('Another prefix (5.6.7); prefix (5.6.7); Algolia for PHP ('.Version::VALUE.')', $userAgent);
     }
 }

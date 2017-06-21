7380e44497468aa381f820894558dfacd59b1c52
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-06-21 15:28:59 +0200

    test(query-rules): add save and get tests

--- tests/AlgoliaSearch/Tests/RulesTest.php
+++ tests/AlgoliaSearch/Tests/RulesTest.php
@@ -5,11 +5,12 @@ namespace AlgoliaSearch\Tests;
 use AlgoliaSearch\AlgoliaException;
 use AlgoliaSearch\Client;
 use AlgoliaSearch\Index;
-use AlgoliaSearch\SynonymType;
 
-class SynonymsTest extends AlgoliaSearchTestCase
+class RulesTest extends AlgoliaSearchTestCase
 {
+    /** @var Client */
     private $client;
+
     /** @var Index */
     private $index;
 
@@ -17,12 +18,8 @@ class SynonymsTest extends AlgoliaSearchTestCase
     {
         $this->client = new Client(getenv('ALGOLIA_APPLICATION_ID'), getenv('ALGOLIA_API_KEY'));
         $this->index = $this->client->initIndex($this->safe_name('àlgol?à-php'));
-
-        try {
-            $this->index->clearIndex();
-        } catch (AlgoliaException $e) {
-            // not fatal
-        }
+        $this->index->clearIndex();
+        $this->index->clearRules();
     }
 
     protected function tearDown()
@@ -30,53 +27,26 @@ class SynonymsTest extends AlgoliaSearchTestCase
         try {
             $this->client->deleteIndex($this->safe_name('àlgol?à-php'));
         } catch (AlgoliaException $e) {
-            // not fatal
         }
     }
 
-    public function testSynonyms()
-    {
-        $res = $this->index->addObject(array('name' => '589 Howard St., San Francisco'));
-        $this->index->waitTask($res['taskID'], 0.1);
-
-        $res = $this->index->batchSynonyms(array(
-            array(
-                'objectID' => 'city',
-                'type'     => 'synonym',
-                'synonyms' => array('San Francisco', 'SF'),
+    public function testSaveAndGetRule() {
+        $rule = array(
+            'objectID' => 'my-rule',
+            'if' => array(
+                'pattern'   => 'some text',
+                'anchoring' => 'is'
             ),
-            array(
-                'objectID'    => 'street',
-                'type'        => 'altCorrection1',
-                'word'        => 'Street',
-                'corrections' => array('St')
+            'then' => array(
+                'params' => array(
+                    'query' => 'other text'
+                )
             )
-        ));
+        );
 
-        $this->index->waitTask($res['taskID'], 0.1);
+        $response = $this->index->saveRule('my-rule', $rule);
+        $this->index->waitTask($response['taskID']);
 
-        $res = $this->index->getSynonym('city');
-        $this->assertEquals('city', $res['objectID']);
-
-        $res = $this->index->search('Howard Street SF');
-        $this->assertEquals(1, $res['nbHits']);
-
-        $res = $this->index->deleteSynonym('street');
-        $this->index->waitTask($res['taskID'], 0.1);
-        $res = $this->index->searchSynonyms('', array(SynonymType::SYNONYM), 0, 5);
-        $this->assertEquals(1, $res['nbHits']);
-
-        $res = $this->index->saveSynonym('city', array(
-            'type'     => 'synonym',
-            'synonyms' => array('San Francisco', 'SF', 'silicon valley')
-        ));
-        $this->index->waitTask($res['taskID'], 0.1);
-        $res = $this->index->search('silicon valley');
-        $this->assertEquals(1, $res['nbHits']);
-
-        $res = $this->index->clearSynonyms();
-        $this->index->waitTask($res['taskID'], 0.1);
-        $res = $this->index->searchSynonyms('', array(), 0, 5);
-        $this->assertEquals(0, $res['nbHits']);
+        $this->assertEquals($rule, $this->index->getRule('my-rule'));
     }
 }

fb25679b4959a51c5c90a15167a92a9f794ee43b
Xavier Grand <grand.xavier@gmail.com>
2017-10-26 15:13:13 +0200

    test(MCM): only use simple quotes

--- tests/AlgoliaSearch/Tests/MultiClusterManagementTest.php
+++ tests/AlgoliaSearch/Tests/MultiClusterManagementTest.php
@@ -32,14 +32,14 @@ class MultiClusterManagementTest extends AlgoliaSearchTestCase
 
         $this->assertTrue($answer['clusters'] !== null);
         $this->assertTrue(count($answer['clusters']) > 0);
-        $this->assertTrue($answer['clusters'][0]["clusterName"] !== null);
+        $this->assertTrue($answer['clusters'][0]['clusterName'] !== null);
         $this->assertTrue($answer['clusters'][0]['nbRecords'] !== null);
         $this->assertTrue($answer['clusters'][0]['nbUserIDs'] !== null);
         $this->assertTrue($answer['clusters'][0]['dataSize'] !== null);
     }
 
     public function testAssignUserID() {
-        $cluster = $this->client->listClusters()["clusters"][0]["clusterName"];
+        $cluster = $this->client->listClusters()['clusters'][0]['clusterName'];
         $answer = $this->client->assignUserID($this->userID, $cluster);
 
         $this->assertTrue($answer['createdAt'] !== null);
@@ -51,35 +51,35 @@ class MultiClusterManagementTest extends AlgoliaSearchTestCase
 
         $this->assertTrue($answer['userIDs'] !== null);
         $this->assertTrue(count($answer['userIDs']) > 0);
-        $this->assertTrue($answer['userIDs'][0]["userID"] !== null);
-        $this->assertTrue($answer['userIDs'][0]["clusterName"] !== null);
-        $this->assertTrue($answer['userIDs'][0]["nbRecords"] !== null);
-        $this->assertTrue($answer['userIDs'][0]["dataSize"] !== null);
+        $this->assertTrue($answer['userIDs'][0]['userID'] !== null);
+        $this->assertTrue($answer['userIDs'][0]['clusterName'] !== null);
+        $this->assertTrue($answer['userIDs'][0]['nbRecords'] !== null);
+        $this->assertTrue($answer['userIDs'][0]['dataSize'] !== null);
     }
 
     public function testGetTopUserID() {
-        $cluster = $this->client->listClusters()["clusters"][0]["clusterName"];
+        $cluster = $this->client->listClusters()['clusters'][0]['clusterName'];
         $answer = $this->client->getTopUserID();
 
         $this->assertTrue($answer['topUsers'] !== null);
         $this->assertTrue(count($answer['topUsers']) > 0);
         $this->assertTrue(count($answer['topUsers'][$cluster]) > 0);
-        $this->assertTrue($answer['topUsers'][$cluster][0]["userID"] !== null);
-        $this->assertTrue($answer['topUsers'][$cluster][0]["nbRecords"] !== null);
-        $this->assertTrue($answer['topUsers'][$cluster][0]["dataSize"] !== null);
+        $this->assertTrue($answer['topUsers'][$cluster][0]['userID'] !== null);
+        $this->assertTrue($answer['topUsers'][$cluster][0]['nbRecords'] !== null);
+        $this->assertTrue($answer['topUsers'][$cluster][0]['dataSize'] !== null);
     }
 
     public function testGetUserID() {
         $answer = $this->client->getUserID($this->userID);
 
-        $this->assertTrue($answer["userID"] !== null);
-        $this->assertTrue($answer["clusterName"] !== null);
-        $this->assertTrue($answer["nbRecords"] !== null);
-        $this->assertTrue($answer["dataSize"] !== null);
+        $this->assertTrue($answer['userID'] !== null);
+        $this->assertTrue($answer['clusterName'] !== null);
+        $this->assertTrue($answer['nbRecords'] !== null);
+        $this->assertTrue($answer['dataSize'] !== null);
     }
 
     public function testSearchUserIDs() {
-        $cluster = $this->client->listClusters()["clusters"][0]["clusterName"];
+        $cluster = $this->client->listClusters()['clusters'][0]['clusterName'];
         $answer = $this->client->searchUserIDs($this->userID, $cluster, 0, 1000);
 
         $this->assertTrue($answer['hits'] !== null);
@@ -87,10 +87,10 @@ class MultiClusterManagementTest extends AlgoliaSearchTestCase
         $this->assertTrue($answer['page'] == 0);
         $this->assertTrue($answer['hitsPerPage'] == 1000);
         $this->assertTrue(count($answer['hits']) > 0);
-        $this->assertTrue($answer['hits'][0]["userID"] !== null);
-        $this->assertTrue($answer['hits'][0]["clusterName"] !== null);
-        $this->assertTrue($answer['hits'][0]["nbRecords"] !== null);
-        $this->assertTrue($answer['hits'][0]["dataSize"] !== null);
+        $this->assertTrue($answer['hits'][0]['userID'] !== null);
+        $this->assertTrue($answer['hits'][0]['clusterName'] !== null);
+        $this->assertTrue($answer['hits'][0]['nbRecords'] !== null);
+        $this->assertTrue($answer['hits'][0]['dataSize'] !== null);
     }
 
     public function testRemoveUserID() {

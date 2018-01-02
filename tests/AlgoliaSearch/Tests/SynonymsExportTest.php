ee73ac2ee8c5c7b081d0f8e33c0f1ae601e3b06b
Julien Bourdeau <julien@sigerr.org>
2018-01-02 17:32:50 +0100

    Build community pull requests

--- tests/AlgoliaSearch/Tests/SynonymsExportTest.php
+++ tests/AlgoliaSearch/Tests/SynonymsExportTest.php
@@ -15,7 +15,14 @@ class SynonymsExportTest extends AlgoliaSearchTestCase
     /** @var Index */
     private $index;
 
-    private $indexName = 'test-synonym-export-php';
+    private $indexName;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->indexName = $this->safe_name('test-synonym-export-php');
+    }
 
     protected function setUp()
     {

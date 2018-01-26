d86e3fe9ce884125dc98905874314ab1feb313bc
Julien Bourdeau <julien@sigerr.org>
2018-01-26 14:09:47 +0100

    Introduce API Wrapper class Relies on httplug interfaces

---
+++ src/ApiWrapper.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Algolia\AlgoliaSearch;
+
+use Http\Client\Exception;
+use Http\Client\HttpClient;
+use Http\Message\MessageFactory;
+use Psr\Http\Message\ResponseInterface;
+
+class ApiWrapper
+{
+    private $applicationId;
+
+    private $apiKey;
+
+    /**
+     * @var HttpClient
+     */
+    private $httpClient;
+
+    /**
+     * @var MessageFactory
+     */
+    private $messageFactory;
+
+    private $validHeaders = [
+        'X-Algolia-Application-Id',
+        'X-Algolia-API-Key',
+        'X-Forwarded-For',
+        'X-Algolia-UserToken',
+        'X-Forwarded-API-Key',
+        'Content-type',
+    ];
+
+    public function __construct($applicationId, $apiKey, HttpClient $httpClient, MessageFactory $messageFactory)
+    {
+        $this->applicationId = $applicationId;
+        $this->apiKey = $apiKey;
+
+        $this->httpClient = $httpClient;
+        $this->messageFactory = $messageFactory;
+    }
+
+    public function get($endpoint, $requestOptions = [], $urlParams = [])
+    {
+        return $this->request('GET', $endpoint, $requestOptions, $urlParams);
+    }
+
+    protected function request($method, $endpoint, $requestOptions = [], $urlParams = [])
+    {
+        [$headers, $body] = $this->splitRequestOptions($requestOptions);
+
+        try {
+            // TODO: Use an super cool custom UriBuilder
+            $uri = $this->gimmeUri($endpoint, $urlParams);
+
+            $request = $this->messageFactory->createRequest($method, $uri, $headers, $body);
+            $response = $this->httpClient->sendRequest($request);
+        } catch (\Exception $e) {
+            dump($e);die;
+        } catch (Exception $e) {
+            dump($e);die;
+        } finally {
+            return $this->formatResponse($response);
+        }
+    }
+
+    protected function formatResponse(ResponseInterface $response)
+    {
+        if (is_null($response)) {
+            throw new \Exception('unreachable hosts');
+        }
+
+        // TODO: handle all status codes
+        if ($response->getStatusCode() != 200) {
+            throw new \Exception((string) $response->getBody());
+        }
+
+        return \GuzzleHttp\json_decode($response->getBody(), true);
+    }
+
+    protected function splitRequestOptions($requestOptions)
+    {
+        $headers = [
+            'X-Algolia-Application-Id' => $this->applicationId,
+            'X-Algolia-API-Key' => $this->apiKey,
+        ];
+
+        $body = [];
+        foreach ($requestOptions as $optionName => $optionValue) {
+            if ($this->isValidHeader($optionName)) {
+                $headers[$optionName] = $optionValue;
+            } else {
+                $body[$optionName] = $optionValue;
+            }
+        }
+
+        // TODO: Set default headers
+
+        return [$headers, \GuzzleHttp\json_encode($body)]; // TODO: build my own json_encode
+    }
+
+    private function isValidHeader($optionName)
+    {
+        return in_array($optionName, $this->validHeaders);
+    }
+
+    private function gimmeUri($endpoint, $urlParams)
+    {
+        $host = 'https://'.$this->applicationId.'-dsn.algolia.net';
+
+        foreach ($urlParams as $key => $value) {
+            if (gettype($value) == 'array') {
+                $urlParams[$key] = json_encode($value);
+            }
+        }
+
+        return $host.$endpoint.'?'.http_build_query($urlParams);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,9 @@
 
 namespace Algolia\AlgoliaSearch\Tests;
 
+use Algolia\AlgoliaSearch\ApiWrapper;
+use Algolia\AlgoliaSearch\Client;
+use Http\Message\MessageFactory\GuzzleMessageFactory;
 use PHPUnit\Framework\TestCase as PHPUitTestCase;
 
 abstract class TestCase extends PHPUitTestCase
@@ -9,5 +12,17 @@ abstract class TestCase extends PHPUitTestCase
     public function safeName($name)
     {
         return $name;
+    }
+
+    protected function getClient()
+    {
+        $wrapper = new ApiWrapper(
+            getenv('ALGOLIA_APP_ID'),
+            getenv('ALGOLIA_API_KEY'),
+            new \Http\Adapter\Guzzle6\Client(),
+            new GuzzleMessageFactory()
+        );
+
+        return new Client($wrapper);
     }
 }

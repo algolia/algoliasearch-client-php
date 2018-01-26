da6508d9898999747ed3a50706dc2279b8cb9d68
Julien Bourdeau <julien@sigerr.org>
2018-01-26 18:27:39 +0100

    Introduce Client::create()
        It easily allows you to get a client with the
        default ApiWrapper (Guzzle)

--- src/Client.php
+++ src/Client.php
@@ -3,6 +3,8 @@
 namespace Algolia\AlgoliaSearch;
 
 use Algolia\AlgoliaSearch\Contracts\ClientInterface;
+use Http\Message\MessageFactory\GuzzleMessageFactory;
+use Http\Message\UriFactory\GuzzleUriFactory;
 
 class Client implements ClientInterface
 {
@@ -15,6 +17,29 @@ class Client implements ClientInterface
     {
         $this->api = $apiWrapper;
     }
+
+    public static function create($appId, $apiKey, $hosts = null)
+    {
+        if (is_null($hosts)) {
+            $hosts = ClusterHosts::createFromAppId($appId);
+        } elseif (is_string($hosts)) {
+            $hosts = new ClusterHosts([$hosts]);
+        } elseif (is_array($hosts)) {
+            $hosts = new ClusterHosts($hosts);
+        }
+
+        $apiWrapper = new ApiWrapper(
+            $appId,
+            $apiKey,
+            $hosts,
+            new \Http\Adapter\Guzzle6\Client(),
+            new GuzzleMessageFactory(),
+            new GuzzleUriFactory()
+        );
+
+        return new static($apiWrapper);
+    }
+
     public function listIndices($requestOptions = [])
     {
         return $this->api->get('/1/indexes/', $requestOptions);

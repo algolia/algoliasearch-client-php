122fa9a0f2285764fc91b9c65922268822840258
Presta Module <support@presta-module.com>
2018-03-29 16:04:37 +0200

    Add missing return statement in isAlive method

--- src/AlgoliaSearch/Client.php
+++ src/AlgoliaSearch/Client.php
@@ -199,7 +199,7 @@ class Client
      */
     public function isAlive()
     {
-        $this->request(
+        return $this->request(
             $this->context,
             'GET',
             '/1/isalive',

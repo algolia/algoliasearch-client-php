b5309a49398ba009976b7938f390852958c0345b
Julien Bourdeau <julien@sigerr.org>
2018-04-12 11:28:02 +0200

    :round_pushpin: Introduce Places Index

---
+++ src/Places.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Algolia\AlgoliaSearch;
+
+use Algolia\AlgoliaSearch\Http\Guzzle6HttpClient;
+use Algolia\AlgoliaSearch\Internals\ApiWrapper;
+use Algolia\AlgoliaSearch\Internals\ClusterHosts;
+use Algolia\AlgoliaSearch\Internals\RequestOptionsFactory;
+use GuzzleHttp\Client as GuzzleClient;
+
+class Places
+{
+    /**
+     * @var ApiWrapper
+     */
+    private $apiWrapper;
+
+    public function __construct(ApiWrapper $apiWrapper)
+    {
+        $this->apiWrapper = $apiWrapper;
+    }
+
+    public static function create($appId = null, $apiKey = null, $hosts = null)
+    {
+        if (is_null($hosts)) {
+            $hosts = ClusterHosts::createForPlaces();
+        } elseif (is_string($hosts)) {
+            $hosts = new ClusterHosts([$hosts]);
+        } elseif (is_array($hosts)) {
+            $hosts = new ClusterHosts($hosts);
+        }
+
+        $apiWrapper = new ApiWrapper(
+            $hosts,
+            new RequestOptionsFactory($appId, $apiKey),
+            new Guzzle6HttpClient(new GuzzleClient)
+        );
+
+        return new static($apiWrapper);
+    }
+}

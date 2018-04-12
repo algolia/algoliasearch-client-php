e98298df8ee353dfb63649e72af74f6a4adce231
Julien Bourdeau <julien@sigerr.org>
2018-04-12 11:28:02 +0200

    :round_pushpin: Introduce Places Index

--- src/Internals/ClusterHosts.php
+++ src/Internals/ClusterHosts.php
@@ -35,6 +35,28 @@ class ClusterHosts
         return new static($hosts);
     }
 
+    public static function createForPlaces()
+    {
+        $read = $write = [
+            'places-1.algolianet.com',
+            'places-2.algolianet.com',
+            'places-3.algolianet.com',
+        ];
+
+        shuffle($read);
+        array_unshift($read, 'places-dsn.algolia.net');
+
+        shuffle($write);
+        array_unshift($write, 'places-dsn.algolia.net');
+
+        $hosts = [
+            'read' => $read,
+            'write' => $write,
+        ];
+
+        return new static($hosts);
+    }
+
     public function read()
     {
         return $this->hosts['read'];

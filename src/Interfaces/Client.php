7573ab20b9d04827b6b88744c0931393c451047b
Julien Bourdeau <julien@sigerr.org>
2018-04-12 14:03:27 +0200

    Renamed namespaces

--- src/Interfaces/Client.php
+++ src/Interfaces/Client.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Algolia\AlgoliaSearch\Contracts;
+namespace Algolia\AlgoliaSearch\Interfaces;
 
-interface ClientInterface
+interface Client
 {
 //    public function multipleQueries($queries, $strategy, $requestOptions);
 //

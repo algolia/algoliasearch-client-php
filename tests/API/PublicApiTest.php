bf99fb159413d123512b7f23a6e8ba89d1e19d9f
Julien Bourdeau <julien@sigerr.org>
2018-04-13 12:04:11 +0200

    Introduce Public API tests :tada:

---
+++ tests/API/PublicApiTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Tests\API;
+
+use Algolia\AlgoliaSearch\Client;
+use Algolia\AlgoliaSearch\Internals\ApiWrapper;
+use Algolia\AlgoliaSearch\Tests\TestCase;
+use Symfony\Component\Yaml\Yaml;
+
+class PublicApiTest extends TestCase
+{
+    public function testClient()
+    {
+        $apiWrapper = $this->createMock(ApiWrapper::class);
+        $client = new Client($apiWrapper);
+        $definition = $this->getDefinition();
+
+        $c = new PublicApiChecker($client, $definition);
+        $c->check();
+    }
+
+    private function getDefinition()
+    {
+        $definition = Yaml::parse(file_get_contents(__DIR__.'/client.yaml'));
+
+        foreach ($definition as &$def) {
+            if (! isset($def['args'])) {
+                $def['args'] = array();
+            }
+        }
+
+        return $definition;
+    }
+}

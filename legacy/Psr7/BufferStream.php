d72886ecc347feef311665a68f581851ec945c1b
Julien Bourdeau <julien@sigerr.org>
2018-04-16 18:06:20 +0200

    Autoload legacy folder

--- legacy/Psr7/BufferStream.php
+++ legacy/Psr7/BufferStream.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Algolia\AlgoliaSearch\Legacy;
+namespace Algolia\AlgoliaSearch\Legacy\Psr7;
 
 use Psr\Http\Message\StreamInterface;
 
--- legacy/Psr7/Request.php
+++ legacy/Psr7/Request.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Algolia\AlgoliaSearch\Legacy;
+namespace Algolia\AlgoliaSearch\Legacy\Psr7;
 
 use InvalidArgumentException;
 use Psr\Http\Message\RequestInterface;

f4f2aaee089445823d217c178f3e14b900458825
Julien Bourdeau <julien@sigerr.org>
2018-05-18 17:31:31 +0200

    Introduce Algolia build_query helper function
    Algolia requires array to be json encoded before being transformed as a query string

--- src/functions.php
+++ src/functions.php
@@ -27,3 +27,32 @@ function api_path($pathFormat, $args = null, $_ = null) {
 
     return call_user_func_array('sprintf', $arguments);
 }
+
+/**
+ * When building a query string, array values must be json_encoded.
+ * This function can be used to turn any array into a Algilia-valid query string
+ *
+ * Do not use a typical implementation where ['key' => ['one', 'two']] is
+ * turned into key[1]=one&key[2]=two. Algolia will not understand key[x].
+ * It should be turned into key=['one','two'] (before being url_encoded).
+ *
+ * @param array $args
+ * @return string The urlencoded query string to send to Algolia
+ */
+function build_query(array $args) {
+    if (!$args) {
+        return '';
+    }
+
+    $args = array_map(function ($value) {
+        if (is_array($value)) {
+            return json_encode($value);
+        } elseif (is_bool($value)) {
+            return $value ? 'true' : 'false';
+        } else {
+            return $value;
+        }
+    }, $args);
+
+    return http_build_query($args);
+}

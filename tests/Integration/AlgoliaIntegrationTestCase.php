b57a1657d092d3fda9dabf934071887128fd0fc7
Julien Bourdeau <julien@sigerr.org>
2018-05-28 10:37:23 +0200

    Introduce airpots dataset for tests

--- tests/Integration/AlgoliaIntegrationTestCase.php
+++ tests/Integration/AlgoliaIntegrationTestCase.php
@@ -48,4 +48,63 @@ abstract class AlgoliaIntegrationTestCase extends PHPUitTestCase
             Client::create($config['app-id'], $config['key'], $config['hosts'])
         );
     }
+
+    public $airports = array(
+        array(
+            "name" => "Chicago Ohare Intl",
+            "city" => "Chicago",
+            "country" => "United States",
+            "iata_code" => "ORD",
+            "_geoloc" => array(
+                "lat" => 41.978603,
+                "lng" => -87.904842
+            ),
+            "links_count" => 1108,
+            "objectID" => "3830",
+        ), array(
+            "name" => "Capital Intl",
+            "city" => "Beijing",
+            "country" => "China",
+            "iata_code" => "PEK",
+            "_geoloc" => array(
+                "lat" => 40.080111,
+                "lng" => 116.584556
+            ),
+            "links_count" => 1069,
+            "objectID" => "3364",
+        ), array(
+            "name" => "Heathrow",
+            "city" => "London",
+            "country" => "United Kingdom",
+            "iata_code" => "LHR",
+            "_geoloc" => array(
+                "lat" => 51.4775,
+                "lng" => -0.461389
+            ),
+            "links_count" => 1051,
+            "objectID" => "507",
+        ), array(
+            "name" => "Charles De Gaulle",
+            "city" => "Paris",
+            "country" => "France",
+            "iata_code" => "CDG",
+            "_geoloc" => array(
+                "lat" => 49.012779,
+                "lng" => 2.55
+            ),
+            "links_count" => 1041,
+            "objectID" => "1382",
+        ), array(
+            "name" => "Los Angeles Intl",
+            "city" => "Los Angeles",
+            "country" => "United States",
+            "iata_code" => "LAX",
+            "_geoloc" => array(
+                "lat" => 33.942536,
+                "lng" => -118.408075
+            ),
+            "links_count" => 990,
+            "objectID" => "3484",
+        ),
+    );
 }

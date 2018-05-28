82b3a222b71a346308f16ff716a5f940e6bdeb89
Julien Bourdeau <julien@sigerr.org>
2018-05-28 18:22:47 +0200

    Introduce ObjectIterator (with refactor AlgoliaIterator)

---
+++ src/Iterators/ObjectIterator.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Iterators;
+
+class ObjectIterator extends AlgoliaIterator
+{
+    /**
+     * Exporting objects (records) doesn't use the search function but the
+     * browse method, no client-side formatting is required.
+     *
+     * @param array $hit
+     * @return array the exact same $hit
+     */
+    protected function formatHit(array $hit)
+    {
+        return $hit;
+    }
+
+    protected function fetchCurrentPageResults()
+    {
+        $cursor = array();
+        if (isset($this->response['cursor'])) {
+            $cursor['cursor'] = $this->response['cursor'];
+        }
+
+        $this->response = $this->api->read(
+            empty($reqOpts) ? 'GET' : 'POST',
+            \Algolia\AlgoliaSearch\api_path('/1/indexes/%s/browse', $this->indexName),
+            array_merge(
+                $this->requestOptions,
+                $cursor
+            )
+        );
+    }
+}
--- src/Iterators/SynonymIterator.php
+++ src/Iterators/SynonymIterator.php
@@ -4,13 +4,6 @@ namespace Algolia\AlgoliaSearch\Iterators;
 
 class SynonymIterator extends AlgoliaIterator
 {
-    /**
-     * The export method is using search internally, this method
-     * is used to clean the results, like remove the highlight
-     *
-     * @param array $hit
-     * @return array formatted synonym array
-     */
     protected function formatHit(array $hit)
     {
         unset($hit['_highlightResult']);
@@ -18,13 +11,15 @@ class SynonymIterator extends AlgoliaIterator
         return $hit;
     }
 
-    /**
-     * Call Algolia' API to get new result batch
-     */
     protected function fetchCurrentPageResults()
     {
-        $reqOpts = array_merge($this->requestOptions, array('page' => $this->getCurrentPage()));
-
-        $this->response = $this->index->searchSynonyms('', $reqOpts);
+        $this->response = $this->api->read(
+            'POST',
+            api_path('/1/indexes/%s/synonyms/search', $this->indexName),
+            array_merge(
+                $this->requestOptions,
+                array('page' => $this->getCurrentPage())
+            )
+        );
     }
 }

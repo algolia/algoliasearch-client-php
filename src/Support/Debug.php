9c53fb3469351448b20aab3f6f3a60091317506f
Julien Bourdeau <julien@sigerr.org>
2018-07-09 15:39:01 -0700

    Introduce debug mode 🎸

---
+++ src/Support/Debug.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Support;
+
+class Debug
+{
+    private static $debug = false;
+
+    private static $handler;
+
+    public static function isEnabled()
+    {
+        return self::$debug;
+    }
+
+    public static function disable()
+    {
+        self::$debug = false;
+    }
+
+    public static function enable()
+    {
+        if (!is_callable(self::$handler)) {
+            self::setHandler(function () {
+                $args = func_get_args();
+                foreach ($args as $arg) {
+                    if (function_exists('dump')) {
+                        dump($arg);
+                    } else {
+                        var_dump($arg);
+                    }
+                }
+            });
+        }
+
+        self::$debug = true;
+    }
+
+    public static function handle()
+    {
+        forward_static_call_array(self::$handler, func_get_args());
+    }
+
+    public static function setHandler(callable $handler)
+    {
+        self::$handler = $handler;
+    }
+}

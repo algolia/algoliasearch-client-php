637773663725f48d2eb7e3b6ffb743cb8532ecc3
Julien Bourdeau <julien@sigerr.org>
2018-08-17 11:04:08 +0200

    Add setter for ClientConfig

--- src/Support/ClientConfig.php
+++ src/Support/ClientConfig.php
@@ -37,9 +37,21 @@ class ClientConfig
         return $this->config['appId'];
     }
 
+    public function setAppId($appId)
+    {
+        $this->config['appId'] = $appId;
+        return $this;
+    }
+
     public function getApiKey()
     {
         return $this->config['apiKey'];
+    }
+
+    public function setApiKey($apiKey)
+    {
+        $this->config['apiKey'] = $apiKey;
+        return $this;
     }
 
     public function getHosts()
@@ -47,9 +59,21 @@ class ClientConfig
         return $this->config['hosts'];
     }
 
+    public function setHosts($hosts)
+    {
+        $this->config['hosts'] = $hosts;
+        return $this;
+    }
+
     public function getWaitTaskRetry()
     {
         return $this->config['waitTaskRetry'];
+    }
+
+    public function setWaitTaskRetry($waitTaskRetry)
+    {
+        $this->config['waitTaskRetry'] = $waitTaskRetry;
+        return $this;
     }
 
     public function getReadTimeout()
@@ -57,13 +81,31 @@ class ClientConfig
         return $this->config['readTimeout'];
     }
 
+    public function setReadTimeout($readTimeout)
+    {
+        $this->config['readTimeout'] = $readTimeout;
+        return $this;
+    }
+
     public function getWriteTimeout()
     {
         return $this->config['writeTimeout'];
     }
 
+    public function setWriteTimeout($writeTimeout)
+    {
+        $this->config['writeTimeout'] = $writeTimeout;
+        return $this;
+    }
+
     public function getConnectTimeout()
     {
         return $this->config['connectTimeout'];
+    }
+
+    public function setConnectTimeout($connectTimeout)
+    {
+        $this->config['connectTimeout'] = $connectTimeout;
+        return $this;
     }
 }

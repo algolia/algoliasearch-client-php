30fec4b09381835a6c1066646c4fdbc38520568d
Nuno Maduro <enunomaduro@gmail.com>
2018-09-12 15:43:14 +0200

    Adds tests

--- tests/Unit/ClientConfigTest.php
+++ tests/Unit/ClientConfigTest.php
@@ -4,11 +4,23 @@ namespace Algolia\AlgoliaSearch\Tests\Unit;
 
 use Algolia\AlgoliaSearch\Support\ClientConfig;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\AbstractLogger;
 use Psr\Log\NullLogger;
 
 class ClientConfigTest extends TestCase
 {
-    public function testDefaultLogger()
+    private $defaultLogger;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->defaultLogger = $this->defaultLogger ?: ClientConfig::getDefaultLogger();
+
+        ClientConfig::setDefaultLogger($this->defaultLogger);
+    }
+
+    public function testLogger()
     {
         $this->assertInstanceOf("Algolia\AlgoliaSearch\Log\Logger", ClientConfig::getDefaultLogger());
 
@@ -17,5 +29,17 @@ class ClientConfigTest extends TestCase
         ClientConfig::setDefaultLogger($logger);
 
         $this->assertSame(ClientConfig::getDefaultLogger(), $logger);
+
+        ClientConfig::setDefaultLogger($this->defaultLogger);
+
+        $config = ClientConfig::create();
+
+        $this->assertSame(ClientConfig::getDefaultLogger(), $config->getLogger());
+
+        $logger = new NullLogger();
+
+        $config->setLogger($logger);
+
+        $this->assertSame($logger, $config->getLogger());
     }
 }

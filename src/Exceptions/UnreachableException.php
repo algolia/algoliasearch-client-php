eb716b2ea0512cbccdb414e880e11cc94f4b12e2
Julien Bourdeau <julien@sigerr.org>
2018-09-26 18:14:26 +0200

    Add type checking for task retry and max retry

--- src/Exceptions/UnreachableException.php
+++ src/Exceptions/UnreachableException.php
@@ -7,7 +7,7 @@ class UnreachableException extends AlgoliaException
     public function __construct($message = '', $code = 0, $previous = null)
     {
         if (!$message) {
-            $message = 'Impossible to connect, please check your Algolia Application Id. Troubleshooting: https://algo.li/unreachable-php';
+            $message = 'Impossible to connect, please check your Algolia Application Id.';
         }
 
         parent::__construct($message, $code, $previous);

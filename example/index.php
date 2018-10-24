c30f3c35cccf859b3d62f2659b37aeea98c53a1f
Shaughn Dolcy <shaughn@dolcy.com>
2018-10-23 21:07:58 -0400

    Minor comment on storing API/APP keys.
    .env is suggested for storing sensitive API/APP keys.

--- example/index.php
+++ example/index.php
@@ -2,6 +2,6 @@
 
 require_once '../algoliasearch.php';
 
-$client = new \AlgoliaSearch\Client('<YOUR_APP_ID>', '<YOUR_API_KEY>');
+$client = new \AlgoliaSearch\Client('<YOUR_APP_ID>', '<YOUR_API_KEY>'); // .env is suggested for storing sensitive API/APP keys.
 var_dump($client->listIndexes());
 var_dump($client->getLogs());

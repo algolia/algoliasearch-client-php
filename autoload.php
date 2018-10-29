6813502b8b5faa1ba47c243cbfbeda6c2632b6cc
Julien Bourdeau <julien@sigerr.org>
2018-10-29 14:01:32 +0100

    Download deps without composer

--- autoload.php
+++ autoload.php
@@ -2,17 +2,22 @@
 
 /*
  * In order to use the client without Composer, you can require
- * this file to load all the lib classes but note that you need
- * to pull https://github.com/php-fig/http-message library in
- * your project and require_once all the classes inside src/
+ * this file to load all the lib classes. You need to install
+ * the other dependencies.
  *
- * The other way is to already have Guzzle loaded
+ * If they are already loaded, ignore the following.
+ *
+ * If you need to download them, use the script
+ * install-dependencies-without-composer in the `bin` folder:
+ *
+ * `php bin/install-dependencies-without-composer`
  */
 
 /*
  * Some helper functions are outside classes and need to be loaded
  */
 require_once './src/functions.php';
+require_once './src/Http/Psr7/functions.php';
 
 /**
  * Based on https://www.php-fig.org/psr/psr-4/examples/.
@@ -42,5 +47,31 @@ spl_autoload_register(function ($class) {
     // if the file exists, require it
     if (file_exists($file)) {
         require $file;
+    }
+});
+
+
+spl_autoload_register(function ($class) {
+    $prefixes = array(
+        'Psr\\Http\\Message\\' => 'psr/http-message/src/',
+        'Psr\\Log\\' => 'psr/log/Psr/Log/',
+        'Psr\\SimpleCache\\' => 'psr/simple-cache/src/',
+    );
+
+    $base_dir = __DIR__.'/vendor/';
+
+    foreach ($prefixes as $prefix => $subdir) {
+        $len = strlen($prefix);
+        if (0 !== strncmp($prefix, $class, $len)) {
+            continue;
+        }
+
+        $relative_class = substr($class, $len);
+
+        $file = $base_dir.$subdir.str_replace('\\', '/', $relative_class).'.php';
+
+        if (file_exists($file)) {
+            require $file;
+        }
     }
 });

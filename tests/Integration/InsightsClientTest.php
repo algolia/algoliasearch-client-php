48d9aa482f2d72e1bd1326166c8dfb09b06f071b
Chloe Liban <chloe.liban@gmail.com>
2020-02-06 11:30:50 +0100

    chore(insights): send_events tests

--- tests/Integration/InsightsClientTest.php
+++ tests/Integration/InsightsClientTest.php
@@ -3,10 +3,16 @@
 namespace Algolia\AlgoliaSearch\Tests\Integration;
 
 use Algolia\AlgoliaSearch\InsightsClient;
-use PHPUnit\Framework\TestCase;
+use Algolia\AlgoliaSearch\SearchClient;
 
-class InsightsClientTest extends TestCase
+class InsightsClientTest extends AlgoliaIntegrationTestCase
 {
+    protected function setUp()
+    {
+        parent::setUp();
+        static::$indexes['insights'] = self::safeName('insights');
+    }
+
     public function testUserInsights()
     {
         $u = InsightsClient::create()->user('userTokenForTest');
@@ -37,6 +43,52 @@ class InsightsClientTest extends TestCase
         $this->assertEvent(
             $u->viewedObjectIDs('eventName', 'indexName', 'objectIDs')
         );
+    }
+
+    public function testSendEvent()
+    {
+        $two_days_ago_ms = (time() - (2 * 24 * 60 * 60)) * 1000;
+        $insightClient = InsightsClient::create();
+        $searchClient = SearchClient::create();
+        $searchClient->initIndex(static::$indexes['insights'])->delete()->wait();
+        $searchClient->initIndex(static::$indexes['insights'])->saveObject(array(
+            'objectID' => 1,
+        ))->wait();
+
+        $response = $insightClient->sendEvent(array(
+                'eventType' => 'click',
+                'eventName' => 'foo',
+                'index' => static::$indexes['insights'],
+                'userToken' => 'bar',
+                'objectIDs' => array('one', 'two'),
+                'timestamp' => $two_days_ago_ms,
+            )
+        );
+
+        $this->assertArraySubset(array('status' => 200), $response);
+    }
+
+    public function testSendEvents()
+    {
+        $two_days_ago_ms = (time() - (2 * 24 * 60 * 60)) * 1000;
+        $insightClient = InsightsClient::create();
+        $searchClient = SearchClient::create();
+        $searchClient->initIndex(static::$indexes['insights'])->delete()->wait();
+        $searchClient->initIndex(static::$indexes['insights'])->saveObject(array(
+            'objectID' => 1,
+        ))->wait();
+
+        $response = $insightClient->sendEvents(array(array(
+                'eventType' => 'click',
+                'eventName' => 'foo',
+                'index' => static::$indexes['insights'],
+                'userToken' => 'bar',
+                'objectIDs' => array('one', 'two'),
+                'timestamp' => $two_days_ago_ms,
+            ))
+        );
+
+        $this->assertArraySubset(array('status' => 200), $response);
     }
 
     private function assertEvent($response)

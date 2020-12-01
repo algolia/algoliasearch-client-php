4b67ceaced6c691eb52af6852bd77ed11342f834
Dries Vints <dries@vints.io>
2020-12-01 10:56:42 +0100

    Rename

--- tests/tests-no-composer.php
+++ tests/tests-no-composer.php
@@ -15,7 +15,7 @@ $index->delete()->wait();
 function safeName($name)
 {
     return sprintf(
-        getenv('CI_BUILD_NUM') ? 'TRAVIS_php_%s_%s_%s' : 'php_%s_%s_%s',
+        getenv('CI_BUILD_NUM') ? 'CIRCLE_php_%s_%s_%s' : 'php_%s_%s_%s',
         date('Y-M-d_H:i:s'),
         getenv('CI_BUILD_NUM') ? getenv('CI_BUILD_NUM') : get_current_user(),
         $name

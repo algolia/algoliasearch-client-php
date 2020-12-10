d492393d612ab983c5767d7ea35bfc878cced802
Dries Vints <dries@vints.io>
2020-12-10 15:17:38 +0100

    Reverse PHPUnit patching

--- .circleci/apply-phpunit-patches.sh
+++ .circleci/apply-phpunit-patches.sh
@@ -6,9 +6,10 @@ set -e
 # Be in the root dir
 cd "$(dirname $0)/../"
 
-find tests/ -type f -print0 | xargs -0 sed -i 's/function setUpBeforeClass(): void/function setUpBeforeClass()/g';
-find tests/ -type f -print0 | xargs -0 sed -i 's/function setUp(): void/function setUp()/g';
-find tests/ -type f -print0 | xargs -0 sed -i 's/function tearDown(): void/function tearDown()/g';
+find tests/ -type f -print0 | xargs -0 sed -i 's/function setUpBeforeClass()/function setUpBeforeClass(): void/g';
+find tests/ -type f -print0 | xargs -0 sed -i 's/function tearDownAfterClass()/function tearDownAfterClass(): void/g';
+find tests/ -type f -print0 | xargs -0 sed -i 's/function setUp()/function setUp(): void/g';
+find tests/ -type f -print0 | xargs -0 sed -i 's/function tearDown()/function tearDown(): void/g';
 
 # Return back to original dir
 cd - > /dev/null

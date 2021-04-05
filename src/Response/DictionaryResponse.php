d4e3112b59bd0488975426b9bd077ad4e1011d7e
Devin Beeuwkes <46448173+DevinCodes@users.noreply.github.com>
2021-04-05 19:44:55 +0200

    feat: custom dictionaries (#662)
    * feat: add custom dictionary methods (tests still WIP)
    
    * chore: add docblocks
    
    * feat: update tests
    
    * debug: use API key dealer
    
    * Revert "debug: use API key dealer"
    
    This reverts commit dcf05df11ae2f4898db16f85788e92e70f9a25ba.
    
    * feat: refactor tests to rely on objectID checks instead of nbHits
    
    * feat: finzalize CTS
    
    * chore: address feedback
    
    * chore: remove unused method

---
+++ src/Response/DictionaryResponse.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Response;
+
+use Algolia\AlgoliaSearch\Config\SearchConfig;
+use Algolia\AlgoliaSearch\RequestOptions\RequestOptions;
+use Algolia\AlgoliaSearch\SearchClient;
+
+final class DictionaryResponse extends AbstractResponse
+{
+    /* @var \Algolia\AlgoliaSearch\SearchClient */
+    private $client;
+
+    /* @var \Algolia\AlgoliaSearch\Config\SearchConfig */
+    private $config;
+
+    /* @var bool */
+    private $done = false;
+
+    /**
+     * DictionaryResponse constructor.
+     */
+    public function __construct(array $apiResponse, SearchClient $client, SearchConfig $config)
+    {
+        $this->apiResponse = $apiResponse;
+        $this->client = $client;
+        $this->config = $config;
+    }
+
+    /**
+     * Wait for the task from this response to finish.
+     *
+     * @param array|RequestOptions $requestOptions
+     *
+     * @return $this
+     */
+    public function wait($requestOptions = array())
+    {
+        $retryCount = 1;
+        $time = $this->config->getWaitTaskTimeBeforeRetry();
+
+        while (!$this->done) {
+            $res = $this->getTask($this->apiResponse['taskID'], $requestOptions);
+
+            if ('published' === $res['status']) {
+                $this->done = true;
+                break;
+            }
+
+            $retryCount++;
+            $factor = ceil($retryCount / 10);
+            usleep($factor * $time); // 0.1 second
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the task details.
+     *
+     * @param int|string           $taskId
+     * @param array|RequestOptions $requestOptions
+     *
+     * @return mixed
+     */
+    private function getTask($taskId, $requestOptions = array())
+    {
+        if (!$taskId) {
+            throw new \InvalidArgumentException('taskID cannot be empty');
+        }
+
+        return $this->client->custom(
+            'GET',
+            \Algolia\AlgoliaSearch\api_path('/1/task/%s', $taskId),
+            $requestOptions
+        );
+    }
+}

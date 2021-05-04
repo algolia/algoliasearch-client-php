b48f1e29bd673e7d7222e6f0ea9b4bcf5dc1afb2
Matthias Günter <1008534+matzeeable@users.noreply.github.com>
2021-05-04 11:03:29 +0200

    fix: use `::class` instead of string for class name (#667)

--- src/functions.php
+++ src/functions.php
@@ -2,7 +2,9 @@
 
 namespace Algolia\AlgoliaSearch;
 
+use Algolia\AlgoliaSearch\Support\Helpers;
+
 function api_path($pathFormat, $args = null, $_ = null)
 {
-    return call_user_func_array(['\Algolia\AlgoliaSearch\Support\Helpers', 'apiPath'], func_get_args());
+    return call_user_func_array([Helpers::class, 'apiPath'], func_get_args());
 }
